486f3d8b75cd6fc62cd15c8a196e4befae9b71e7
xup6890609 <71679918+xup6890609@users.noreply.github.com>
2020-12-22 12:01:49 +0800

    新增布料材質、得分程式

---
+++ ASIA_VR_Basketball/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+  [Header("得分介面")]
+  public Text textScore;
+  [Header("分數")]
+  public int score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}
